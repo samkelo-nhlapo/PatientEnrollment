f149d7ac9e1bf0ff36859fa0ee22799e4cf95b61
Samkelo Michael <72704428+samkelo-nhlapo@users.noreply.github.com>
2022-05-13 00:20:54 +0200

    fixing the app

--- PatientEnrollmentVS/PatientEnrollmentVS/Controllers/HomeController.cs
+++ PatientEnrollmentVS/PatientEnrollmentVS/Controllers/HomeController.cs
@@ -99,23 +99,25 @@ namespace PatientEnrollmentVS.Controllers
                 cmd.Parameters.Add(new SqlParameter("@LastName", locationModel.LastName));
                 cmd.Parameters.Add(new SqlParameter("@ID_Number", locationModel.ID_Number));
                 cmd.Parameters.Add(new SqlParameter("@DateOfBirth", locationModel.DateOfBirth));
-                cmd.Parameters.Add(new SqlParameter("@GenderIDFK", Int32.Parse(locationModel.GenderIdFK)));
+                cmd.Parameters.Add(new SqlParameter("@GenderIDFK", Int32.Parse(locationModel.GenderIDFK)));
 
                 cmd.Parameters.Add(new SqlParameter("@PhoneNumber", locationModel.PhoneNumber));
                 cmd.Parameters.Add(new SqlParameter("@Email", locationModel.Email));
                 cmd.Parameters.Add(new SqlParameter("@Line1", locationModel.Line1));
                 cmd.Parameters.Add(new SqlParameter("@Line2", locationModel.Line2));
-                cmd.Parameters.Add(new SqlParameter("@CityIDFK", Int32.Parse(locationModel.CityIdFK)));
+                cmd.Parameters.Add(new SqlParameter("@CityIDFK", Int32.Parse(locationModel.CityIDFK)));
 
-                cmd.Parameters.Add(new SqlParameter("@ProvinceIDFK", Int32.Parse(locationModel.ProvinceIdFK)));
-                cmd.Parameters.Add(new SqlParameter("@CountryIDFK", Int32.Parse(locationModel.CountryIdFK)));
-                cmd.Parameters.Add(new SqlParameter("@MaritalStatusIDFK", Int32.Parse(locationModel.MaritalStatusIdFK)));
+                cmd.Parameters.Add(new SqlParameter("@ProvinceIDFK", Int32.Parse(locationModel.ProvinceIDFK)));
+                cmd.Parameters.Add(new SqlParameter("@CountryIDFK", Int32.Parse(locationModel.CountryIDFK)));
+                cmd.Parameters.Add(new SqlParameter("@MaritalStatusIDFK", Int32.Parse(locationModel.MaritalStatusIDFK)));
                 cmd.Parameters.Add(new SqlParameter("@MedicationList", locationModel.MedicationList));
                 cmd.Parameters.Add(new SqlParameter("@EmergencyName", locationModel.EmergencyName));
 
                 cmd.Parameters.Add(new SqlParameter("@EmergencyLastName", locationModel.EmergencyLastName));
-                cmd.Parameters.Add(new SqlParameter("@Relationship", locationModel.Relationship));
                 cmd.Parameters.Add(new SqlParameter("@EmergencyPhoneNumber", locationModel.EmergencyPhoneNumber));
+                cmd.Parameters.Add(new SqlParameter("@Relationship", locationModel.Relationship));
+                cmd.Parameters.Add(new SqlParameter("@EmergancyDateOfBirth", locationModel.EmergancyDateOfBirth));
+
 
                 cmd.Parameters.Add(new SqlParameter("@Message", SqlDbType.VarChar.ToString())).Direction = ParameterDirection.Output;
 
--- PatientEnrollmentVS/PatientEnrollmentVS/Models/LocationModel.cs
+++ PatientEnrollmentVS/PatientEnrollmentVS/Models/LocationModel.cs
@@ -9,23 +9,23 @@ namespace PatientEnrollmentVS.Models
     public class LocationModel
     {
         //Country
-        public string CountryIdFK { get; set; }
+        public string CountryIDFK { get; set; }
         public string CountryName { get; set; }
 
         //Province
-        public string ProvinceIdFK { get; set; }
+        public string ProvinceIDFK { get; set; }
         public string ProvinceName { get; set; }
 
         //Cities
-        public string CityIdFK { get; set; }
+        public string CityIDFK { get; set; }
         public string CityName { get; set; }
 
         //MaritalStatus
-        public string MaritalStatusIdFK { get; set; }
+        public string MaritalStatusIDFK { get; set; }
         public string MaritalStatusDescription { get; set; }
 
         //Gender
-        public string GenderIdFK { get; set; }
+        public string GenderIDFK { get; set; }
         public string GenderDescription { get; set; }
 
         //Patient
@@ -34,24 +34,25 @@ namespace PatientEnrollmentVS.Models
         [Required(ErrorMessage = "Enter Your Last Name")]
         public string LastName { get; set; }
         public string ID_Number { get; set; }
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        //[DataType(DataType.Date)]
+        //[DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DateOfBirth { get; set; }
-        [DataType(DataType.PhoneNumber)]
-        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid Phone number")]
+        //[DataType(DataType.PhoneNumber)]
+        //[RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid Phone number")]
         public string PhoneNumber { get; set; }
-        [Required(ErrorMessage = "Enter your Email Address")]
-        [RegularExpression(@"^[\w -\._\+%] +@(?:[\w -] +\.)+[\w]{2,6}$", ErrorMessage = "Please enter a valid email address")]
+        //[Required(ErrorMessage = "Enter your Email Address")]
+        //[RegularExpression(@"^[\w -\._\+%] +@(?:[\w -] +\.)+[\w]{2,6}$", ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
         public string Line1 { get; set; }
         public string Line2 { get; set; }
         public string MedicationList { get; set; }
         public string EmergencyName { get; set; }
         public string EmergencyLastName { get; set; }
-        public string Relationship { get; set; }
-        [DataType(DataType.PhoneNumber)]
-        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid Phone number")]
+        //[DataType(DataType.PhoneNumber)]
+        //[RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid Phone number")]
         public string EmergencyPhoneNumber { get; set; }
+        public string Relationship { get; set; }
+        public DateTime EmergancyDateOfBirth { get; set; }
 
         public string Message { get; set; }
 
